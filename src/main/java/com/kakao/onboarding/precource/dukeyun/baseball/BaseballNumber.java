fb4ceb876a6507fb427d3e77de1718f379ab303f
김덕윤 <duk9741@gmail.com>
2026-01-28 18:00:56 +0900

    feat: 숫자 중복 관련 검증 구현

--- src/main/java/com/kakao/onboarding/precource/dukeyun/baseball/BaseballNumber.java
+++ src/main/java/com/kakao/onboarding/precource/dukeyun/baseball/BaseballNumber.java
@@ -13,7 +13,19 @@ public class BaseballNumber {
 	public BaseballNumber(List<Integer> numbers) {
 		checkLength(numbers);
 		checkNumbersRange(numbers);
+		checkDuplication(numbers);
 		this.numbers = numbers;
+	}
+
+	private void checkDuplication(List<Integer> numbers) {
+		int[] freq = new int[MAX_RANGE - MIN_RANGE + 1];
+		for (int number : numbers) {
+			int index = number - MIN_RANGE;
+			freq[index]++;
+			if (freq[index] > 1) {
+				throw new RuntimeException(String.format("숫자가 중복 되었습니다.(중복 숫자: %d)", number));
+			}
+		}
 	}
 
 	private void checkNumbersRange(List<Integer> numbers) {
--- src/test/java/com/kakao/onboarding/precource/dukeyun/baseball/BaseballNumberTest.java
+++ src/test/java/com/kakao/onboarding/precource/dukeyun/baseball/BaseballNumberTest.java
@@ -1,36 +1,52 @@
 package com.kakao.onboarding.precource.dukeyun.baseball;
 
+import static org.assertj.core.api.Assertions.*;
+
 import java.util.List;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class BaseballNumberTest {
 	@Test
 	void 기준_길이보다_길면_생성이_안된다() {
-		Assertions.assertThatThrownBy(() ->
+		assertThatThrownBy(() ->
 			new BaseballNumber(List.of(1, 2, 3, 4))
 		).isInstanceOf(RuntimeException.class);
 	}
 
 	@Test
 	void 기준_길이보다_짧으면_생성이_안된다() {
-		Assertions.assertThatThrownBy(() ->
+		assertThatThrownBy(() ->
 			new BaseballNumber(List.of(1, 2))
 		).isInstanceOf(RuntimeException.class);
 	}
 
 	@Test
 	void 숫자가_범위가_최소_허용_범위보다_작으면_생성이_안된다() {
-		Assertions.assertThatThrownBy(() ->
+		assertThatThrownBy(() ->
 			new BaseballNumber(List.of(-1, 2, 3))
 		).isInstanceOf(RuntimeException.class);
 	}
 
 	@Test
 	void 숫자가_범위가_최소_허용_범위보다_크면_생성이_안된다() {
-		Assertions.assertThatThrownBy(() ->
+		assertThatThrownBy(() ->
 			new BaseballNumber(List.of(10, 2, 3))
 		).isInstanceOf(RuntimeException.class);
 	}
+
+	@Test
+	void 중복된_숫자로_생성할_수_없다() {
+		assertThatThrownBy(() ->
+			new BaseballNumber(List.of(1, 1, 3))
+		).isInstanceOf(RuntimeException.class);
+	}
+
+	@Test
+	void 올바른_조건으로_생성할_수_있다() {
+		assertThatCode(
+			() -> new BaseballNumber(List.of(1, 2, 3))
+		).doesNotThrowAnyException();
+	}
+
 }
