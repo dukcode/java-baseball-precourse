259b1d3cbbe0767dbd2b093b4145d46b135fd9ca
김덕윤 <duk9741@gmail.com>
2026-02-08 23:09:19 +0900

    feat: 게임 생성을 위한 Factory 클래스 추가
    - Config 기반으로 게임 시스템을 생성하는 팩토리 메서드 제공
    - 기본 설정 및 커스텀 설정 지원

---
+++ src/main/java/com/kakao/onboarding/precource/dukeyun/baseball/GameFactory.java
@@ -0,0 +1,34 @@
+package com.kakao.onboarding.precource.dukeyun.baseball;
+
+import com.kakao.onboarding.precource.dukeyun.baseball.baseball.BaseballGameConfig;
+import com.kakao.onboarding.precource.dukeyun.baseball.baseball.BaseballGameConsoleView;
+import com.kakao.onboarding.precource.dukeyun.baseball.baseball.BaseballGameService;
+import com.kakao.onboarding.precource.dukeyun.baseball.baseball.RandomBaseballNumberGenerator;
+import com.kakao.onboarding.precource.dukeyun.baseball.framework.game.Game;
+import com.kakao.onboarding.precource.dukeyun.baseball.framework.game.turn.TurnBaseGame;
+import com.kakao.onboarding.precource.dukeyun.baseball.framework.gamesystem.GameSystem;
+import com.kakao.onboarding.precource.dukeyun.baseball.framework.gamesystem.ReplayableGameSystem;
+import com.kakao.onboarding.precource.dukeyun.baseball.framework.gamesystem.ReplayableGameSystemConsoleView;
+
+public class GameFactory {
+	private GameFactory() {
+	}
+
+	public static GameSystem createBaseballGameSystem() {
+		return createBaseballGameSystem(new BaseballGameConfig(3, 1, 9));
+	}
+
+	public static GameSystem createBaseballGameSystem(BaseballGameConfig config) {
+		return new ReplayableGameSystem(
+			new ReplayableGameSystemConsoleView(),
+			createBaseBallGame(config)
+		);
+	}
+
+	private static Game createBaseBallGame(BaseballGameConfig config) {
+		return new TurnBaseGame(
+			new BaseballGameConsoleView(config),
+			new BaseballGameService(config, new RandomBaseballNumberGenerator(config))
+		);
+	}
+}
