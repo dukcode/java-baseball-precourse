3b09d5260ac4024369a52c54ca3725692a279d52
김덕윤 <duk9741@gmail.com>
2026-02-09 11:21:41 +0900

    refactor: RandomBaseballNumberGenerator 기본 생성자 삭제

--- src/main/java/com/kakao/onboarding/precource/dukeyun/baseball/baseball/RandomBaseballNumberGenerator.java
+++ src/main/java/com/kakao/onboarding/precource/dukeyun/baseball/baseball/RandomBaseballNumberGenerator.java
@@ -10,10 +10,6 @@ public class RandomBaseballNumberGenerator implements BaseballNumberGenerator {
 
 	private final BaseballGameConfig config;
 
-	public RandomBaseballNumberGenerator() {
-		this(new BaseballGameConfig(3, 1, 9));
-	}
-
 	public RandomBaseballNumberGenerator(BaseballGameConfig config) {
 		this.config = config;
 	}
